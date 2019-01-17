ecb1e205dd8205123bfc508587b1bd3d714f1157
Gajendra KT <gajendrarok@gmail.com>
2019-01-17 11:43:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-//Load express module with `require` directive
-var express = require('express')
-var app = express()
+function disp(){
+	console.log("called");
+	return " good morning"
+}
+function add(x,y){return x+y}
 
-//Define request response in root URL (/)
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-//Launch listening server on port 8080
-app.listen(8000, function () {
-  console.log('App listening on port 8080!')
-})
+module.exports={disp,add};
